fade34e15f233e47511629c51c5bac0f4ed2e43c
tanakakeishiro <tanakakeishiro349489@gmail.com>
2025-12-18 19:13:37 +0900

    feat(index): トップページのレイアウトとコンテンツを実装
    - index.html にヘッダー、メインビジュアル、ニュース、コンセプト、サービス、ブログ、FAQ、フッターを追加
    - 新しい画像ファイルを多数追加（ブログ、カード、コンセプト、SNSアイコンなど）
    - スタイルシートを更新し、フォントやレイアウトを調整
    - jQuery を使用してドロワーメニューの動作を追加
    - 不要な .gitkeep ファイルを削除

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -26,3 +26,64 @@
   // 初回読み込み時にも現在の幅に応じて viewport を設定する
   switchViewport();
 })();
+
+jQuery("#js-drawer-button").on("click", function (e) {
+  e.preventDefault();
+  jQuery("#js-drawer-button").toggleClass("is-checked");
+  jQuery("#js-drawer-content").toggleClass("is-checked");
+});
+
+// sp表示のときにドロワーメニューが開いている状態でリンクをクリックしたときに、ドロワーメニューを閉じるようにするためのコードです。
+jQuery('#js-drawer-content a[href^="#"]').on("click", function (e) {
+  jQuery("#js-drawer-icon").removeClass("is-checked");
+  jQuery("#js-drawer-content").removeClass("is-checked");
+});
+
+$("#js-drawer-button").click(function () {
+  $(".drawer-icon__bar").toggleClass("drawer-icon__color");
+});
+
+const swiper = new Swiper("#js-work-swiper", {
+  // Optional parameters
+  loop: true,
+  // slidesPerView: "auto"に上書きされるから消した。
+  // slidesPerView: 1.25,
+  spaceBetween: 18,
+  //画面幅を変えることができる。
+  slidesPerView: "auto",
+
+  // If we need pagination
+  pagination: {
+    el: "#js-work-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: "#js-work-next",
+    prevEl: "#js-work-prev",
+  },
+});
+
+$(function () {
+  $(".js-content:first-of-type").css("display", "block");
+  $(".js-tab").on("click", function () {
+    $(".current").removeClass("current");
+    $(this).addClass("current");
+    const index = $(this).index();
+    $(".js-content").hide().eq(index).fadeIn(300);
+  });
+});
+
+$(function () {
+  $(".js-accordion").click(function () {
+    //クリックされた質問の子要素のspan以外からはopenというクラスを外す
+    $(".js-accordion").not(this).children("span").removeClass("open");
+    //クリックされた質問部分に対する回答以外は全て閉じる
+    $(".js-accordion").not(this).next().slideUp(400);
+
+    //クリックされた質問の子要素のspanにopenクラスが付与されいなければ付与し、付与されていれば外す
+    $(this).children("span").toggleClass("open");
+    //クリックされた質問に対する回答を表示する
+    $(this).next().slideToggle(400);
+  });
+});
